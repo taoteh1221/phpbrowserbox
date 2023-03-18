1511a6927a2687fff049232fbb3924c19d59cd95
dhtml <diltony@gmail.com>
2023-03-18 21:13:10 +0100

    update

---
+++ cpp/startupcheck.cmd
@@ -0,0 +1,21 @@
+@echo off
+@echo off
+rem change drive
+cd /D "%~dp0"
+
+rem change directory
+cd "%~dp0..\"
+
+
+
+SET maincwd=%~dp0
+
+set "cwd=%cd%"
+
+
+set "apache=%cwd%\bin\apache\bin\httpd.exe"
+set "logs=%cwd%\tmp\startup.txt"
+
+del "%logs%" /f
+
+"%apache%" -M  |find /i "Loaded" > "%logs%"
